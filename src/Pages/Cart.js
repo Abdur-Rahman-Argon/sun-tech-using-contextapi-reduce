0003ca4494742cec3e62eb59d923e9dcd38a99be
Abdur-Rahman-Argon <abdurrahman.me7@gmail.com>
2022-12-12 15:28:50 +0600

    project setup

---
+++ src/Pages/Cart.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Cart = () => {
+  return (
+    <div className=" text-center font-bold my-10">
+      {" "}
+      <h1> this is Cart</h1>
+    </div>
+  );
+};
+
+export default Cart;
---
+++ src/Pages/Home.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
+
+const Home = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"></div>
+  );
+};
+
+export default Home;

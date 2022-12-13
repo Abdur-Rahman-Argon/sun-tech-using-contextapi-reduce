c22e22bc46c8b26da9acef61e03807a296819508
Abdur-Rahman-Argon <abdurrahman.me7@gmail.com>
2022-12-13 10:00:15 +0600

    Using Reducer

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,10 +3,12 @@ import ProductCard from "../components/ProductCard";
 import { useProduct } from "../context/ProductProvider";
 
 const Home = () => {
-  const context = useProduct();
+  const {
+    state: { products },
+  } = useProduct();
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-      {context?.products?.map((product) => (
+      {products?.map((product) => (
         <ProductCard key={product._id} product={product} />
       ))}
     </div>
--- src/context/ProductProvider.js
+++ src/context/ProductProvider.js
@@ -1,18 +1,30 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect, useReducer } from "react";
+import { actionTypes } from "../state/ProductState/actionTypes";
+import {
+  initialState,
+  productReducer,
+} from "../state/ProductState/ProductReducer";
 
 export const PRODUCT_CONTEXT = createContext();
 
 const ProductProvider = ({ children }) => {
-  const [products, setProducts] = useState([]);
+  const [state, dispatch] = useReducer(productReducer, initialState);
 
   useEffect(() => {
+    dispatch({ type: actionTypes.FETCHING_START });
     fetch("http://localhost:5000/product")
       .then((res) => res.json())
-      .then((data) => setProducts(data));
+      .then((data) =>
+        dispatch({ type: actionTypes.FETCHING_SUCCESS, payload: data })
+      )
+      .catch(() => dispatch({ type: actionTypes.FETCHING_ERROR }));
   }, []);
 
+  console.log(state);
+
   const value = {
-    products,
+    state,
+    dispatch,
   };
 
   return (
